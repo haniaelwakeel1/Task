e54b99a71cedf382d89f4896ef6dceecf75129af
Hania Elwakeel <haniaelwakeel1@gmail.com>
2024-10-25 02:18:14 +0300

    first commit

---
+++ middleware/auth.js
@@ -0,0 +1,14 @@
+import jwt from 'jsonwebtoken';
+
+const JWT_SECRET = 'your_secret_key'; 
+
+export const authenticateToken = (req, res, next) => {
+  const token = req.headers['authorization'];
+  if (!token) return res.status(401).json({ message: 'Unauthorized' });
+
+  jwt.verify(token, JWT_SECRET, (err, user) => {
+    if (err) return res.status(403).json({ message: 'Invalid token' });
+    req.user = user;
+    next();
+  });
+};
