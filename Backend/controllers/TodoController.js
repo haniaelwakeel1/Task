452201dd78ad2f049847e293e66406d4e62a92eb
haniaelwakeel1 <haniaelwakeel1@gmail.com>
2024-10-26 13:24:39 +0300

    Add files via upload

--- Backend/controllers/TodoController.js
+++ Backend/controllers/TodoController.js
@@ -1,77 +1,97 @@
+
+
+import Joi from 'joi';
 import prisma from '../Prisma/client.js';
-    // List all todos
+
+const Status = {
+  PENDING: 'PENDING',
+  COMPLETED: 'COMPLETED',
+  CANCELLED: 'CANCELLED',
+}
+
+export const addTodo = async (req, res) => {
+  try {
+    const schema = Joi.object({
+      title: Joi.string().required(),
+    })
+    const { error } = schema.validate(req.body);
+    if (error) return res.status(400).json({ error: error.details[0].message });
+
+    const { title } = req.body;
+    const { userId } = req.user;
+
+    const pendingTodo = await prisma.todo.findFirst({
+      where: {
+        title,  
+        userId
+      }
+    });
+    if (pendingTodo) return res.status(400).json({ message: 'Todo already exists' });
+  
+    const todo = await prisma.todo.create({
+      data: {
+        title,
+        status: Status.PENDING,
+        userId
+      },
+    });
+  
+    res.status(201).json(todo);
+  } catch (error) {
+    console.error('Error adding todo:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+}; 
 
 export const getTodos = async (req, res) => {
-    const todos = await prisma.todo.findMany({
-      where: { userId: req.user.userId },
-    });
-    res.json(todos);
-  };
-
-  // Add a new todo
-export const addTodo = async (req, res) => {
-  const { title } = req.body;
-  const todo = await prisma.todo.create({
-    data: {
-      title,
-      status: 'pending',
-      userId: req.user.userId
-    },
-  });
-  res.status(201).json(todo);
-};  
-// export const updateTodo = async (req, res) => {
-//   const { id } = req.params;
-//   const { status } = req.body;
-  
-// const data = { status }
-// const resolvedStatuses = ['COMPLETED', 'CANCELLED']
-// if (resolvedStatuses.includes(status)) {
-//     data.resolvedAt = new Date()
-// }
-//  const todo = await prisma.todo.update({
-//     where: { id },
-//     data,
-//   });
-//   res.json(todo);
-// };
-
-
-// export const updateTodo = async (todoId, newStatus) => {
-//   // if (!token) return; // Ensure the user is authenticated
-
-//   const response = await prisma.todo.update(
-//     `http://localhost:5000/todo/${todoId}/status`, 
-//     { status: newStatus }// This is the request body
-   
-//   );
-
-//   return response.data; // Return the updated todo item
-// };
-
-// export const updateTodo = async (todoId, newStatus) => {
-//   console.log('todoId:', todoId, 'newStatus:', newStatus); 
-//   try {
-//     const updatedTodo = await prisma.todo.update({
-//       where: { id: todoId },          
-//       data: { status: newStatus },     
-//     });
-//     return updatedTodo; 
-//   } catch (error) {
-//     console.error('Error updating todo:', error.message);
-//     throw error; 
-//   }
-// };
-export const updateTodo = async (todoId, newStatus) => {
   try {
-    const updatedTodo = await prisma.todo.update({
-      where: { id: parseInt(todoId) },  
-      data: { status: newStatus },      
+    const { userId } = req.user;
+
+    const todos = await prisma.todo.findMany({
+      where: { userId }
     });
-    
-    return updatedTodo;  
+
+    res.json(todos);
   } catch (error) {
-    console.error('Error updating todo:', error.message);
-    throw error; 
-  }
+    console.error('Error getting todos:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+ 
+export const updateTodoStatus = async (req, res) => {
+  try {
+    const schema = Joi.object({
+      id: Joi.string().required(),
+      status: Joi.string().valid(...Object.values(Status)).required()
+    });
+    const { error } = schema.validate({ ...req.params, ...req.body });
+    if (error) return res.status(400).json({ error: error.details[0].message });
+
+    const { id } = req.params;
+    const { status } = req.body;
+    const { userId } = req.user;
+
+    const todo = await prisma.todo.findFirst({
+      where: {
+        id: parseInt(id),
+        userId,
+        
+      },
+    });
+    if (!todo) return res.status(404).json({ message: 'Todo not found' });
+    if (status === todo.status) return res.status(400).json({ message: 'Sent status is same as current status' });
+
+    const data = { status };
+    const resolvingStatuses = [Status.COMPLETED, Status.CANCELLED];
+  
+    const updatedTodo = await prisma.todo.update({
+      where: { id: todo.id },
+      data
+    });
+
+    res.json(updatedTodo);
+  } catch (error) {
+    console.error('Error updating todo:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
 };
